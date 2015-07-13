c5879a03cbc5e589959d056dfea2441d57d8814a
petfle <peter.tollnes.flem@knowit.no>
2015-07-13 12:04:07 +0200

    Fixed a typo and a bug in the code, now testing express.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var WebSocketServer = require("ws").Server;
-var app = requrie('express')();
+var app = require('express')();
 var port = process.env.PORT || 5000;
 var server = require("http").createServer(app);
 
 app.get('/', function (req, res) {
-  res.send(200).send('Hello!');
+  res.status(200).send('Hello!');
 });
 
 server.listen(port);
