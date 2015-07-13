6a2f0560ac9da3859a00eda910b3005bd1ff8ed3
petfle <peter.tollnes.flem@knowit.no>
2015-07-13 12:01:06 +0200

    Added and testing express

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var WebSocketServer = require("ws").Server;
+var app = requrie('express')();
 var port = process.env.PORT || 5000;
-var server = require("http").createServer(function(){});
+var server = require("http").createServer(app);
+
+app.get('/', function (req, res) {
+  res.send(200).send('Hello!');
+});
 
 server.listen(port);
 
