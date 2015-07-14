3fb86c1b07a1f0f026ddb8ddaeaadcf39ca6974b
petfle <peter.tollnes.flem@knowit.no>
2015-07-14 15:54:03 +0200

    Trying to log specific property in the post request

--- app/input_modules/team-city.js
+++ app/input_modules/team-city.js
@@ -2,7 +2,7 @@ module.exports.initialize = function (callback, app) {
   app.post('/api/team-city', function (req, res) {
     callback('masse data');
     console.log('========Team City hit!!===========');
-    console.log(req.body);
+    console.log(req.body.test);
     console.log('==================================');
     res.sendStatus(200);
   });
