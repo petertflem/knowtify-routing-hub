26ba5bd8e72cf965e8a734f7059f798381d76c40
petfle <peter.tollnes.flem@knowit.no>
2015-07-14 15:27:39 +0200

    Added debug logging to the team city endpoint.

--- app/input_modules/team-city.js
+++ app/input_modules/team-city.js
@@ -1,6 +1,7 @@
 module.exports.initialize = function (callback, app) {
   app.get('/api/team-city', function (req, res) {
     callback('masse data');
+    console.log('Team City hit!!');
     res.sendStatus(200);
   });
 };
