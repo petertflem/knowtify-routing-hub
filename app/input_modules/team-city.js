3b947b43b3bf0a08ad758ac2568e3910fff2de38
petfle <peter.tollnes.flem@knowit.no>
2015-07-14 15:33:26 +0200

    Now logging the incoming request in the team city endpoint.

--- app/input_modules/team-city.js
+++ app/input_modules/team-city.js
@@ -2,6 +2,8 @@ module.exports.initialize = function (callback, app) {
   app.get('/api/team-city', function (req, res) {
     callback('masse data');
     console.log('Team City hit!!');
+    console.log(req);
+
     res.sendStatus(200);
   });
 };
